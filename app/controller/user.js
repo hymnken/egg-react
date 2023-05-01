700cbaa6669049ab9b86cb458bb74fc52d5c2b67
hymnken <hymnken@yeah.net>
2023-05-01 23:05:34 +0800

    开发用户登录接口

--- app/controller/user.js
+++ app/controller/user.js
@@ -2,7 +2,7 @@ const Controller = require("egg").Controller;
 const md5 = require("md5");
 const dayjs = require("dayjs");
 class UserController extends Controller {
-  // 注册
+  // 注册接口
   async register() {
     const { ctx, app } = this;
     const params = ctx.request.body;
@@ -18,20 +18,42 @@ class UserController extends Controller {
       ...params,
       // 隐式加密 只有开发人员才知道后面加了个hymnken
       password: md5(params.password + app.config.salt),
-      createTime: ctx.helper.time()
+      createTime: ctx.helper.time(),
     });
     if (result) {
       ctx.body = {
         status: 200,
         data: {
-          ...ctx.helper.unPick(result.dataValues, ['password']),
-          createTime:ctx.helper.timeStamp(result.createTime)
-        }
+          ...ctx.helper.unPick(result.dataValues, ["password"]),
+          createTime: ctx.helper.timeStamp(result.createTime),
+        },
       };
     } else {
       ctx.body = {
         status: 500,
         errMsg: "注册用户失败",
+      };
+    }
+  }
+
+  // 登录接口
+  async login() {
+    const { ctx } = this;
+    const { username, password } = ctx.request.body;
+    const user = await ctx.service.user.getUser(username, password);
+    if (user) {
+      ctx.session.userId = user.id;
+      ctx.body = {
+        status: 200,
+        data: {
+          ...ctx.helper.unPick(user.dataValues, ["password"]),
+          createTime: ctx.helper.timeStamp(user.createTime),
+        },
+      };
+    } else {
+      ctx.body = {
+        status: 500,
+        errMsg: "该用户不存在",
       };
     }
   }
